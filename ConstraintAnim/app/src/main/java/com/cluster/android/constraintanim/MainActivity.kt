35e50e954b1e73c34f28cef955f641208f49c272
saperez <saperez@unicauca.edu.co>
2018-12-23 21:16:02 -0500

    with Databinding

--- ConstraintAnim/app/src/main/java/com/cluster/android/constraintanim/MainActivity.kt
+++ ConstraintAnim/app/src/main/java/com/cluster/android/constraintanim/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.cluster.android.constraintanim
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.transition.TransitionManager
+import com.cluster.android.constraintanim.databinding.ActivityMainExpandedBinding
+import com.cluster.android.constraintanim.models.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,21 +19,34 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_expanded)
+        val binding:ActivityMainExpandedBinding = DataBindingUtil.setContentView(this, R.layout.activity_main_expanded)
 
         setExpanded.clone(root)
         setCollapsed.clone(this, R.layout.activity_main)
 
 
         //Modelo que exprese los pasos para insertar (inyectar)
+        /* Con el data binding no son necesarias las siguientes lineas de codigo
         today.text = "12 de Diciembre"
         time.text = "2 horas"
 
         des.text = "La mejor pelicula..."
         name.text = "Interestellar"
         img.setImageURI("https://i1.wp.com/asdecopas.cl/wp-content/uploads/2014/11/interstellar.jpg?w=960")
+        */
+
+        //Objeto de tipo "Movie"
+        val movie = Movie("Intererestellar",
+            "https://i1.wp.com/asdecopas.cl/wp-content/uploads/2014/11/interstellar.jpg?w=960",
+            "12 de Diciembre",
+            "2 hours",
+            "Best movie")
+
+        binding.movie = movie
+
 
         //Implementando un lambda (function type)
         btnInfo.setOnClickListener{
---
+++ ConstraintAnim/app/src/main/java/com/cluster/android/constraintanim/models/Movie.kt
@@ -0,0 +1,13 @@
+package com.cluster.android.constraintanim.models
+
+import java.time.Duration
+
+//El paquete models contendra los elementos que van a presentar la informacion
+//La primera sera "movie"
+
+class Movie(
+        val name:String,
+        val img: String,
+        val date: String,
+        val duration: String,
+        val description: String)
